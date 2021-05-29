3bf484cd9f524be5158ca2fcd1a660f826a2b86d
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-05-30 00:10:46 +0300

    add actor with status

---
+++ ActorWithStatus/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Akka;
+using Akka.Actor;
+
+namespace ActorWithStatus
+{
+    class Program
+    {
+        static void Main()
+        {
+            using var systemActor = ActorSystem.Create("my-system");
+            var actorProps = Props.Create<ActorWithStatus>();
+            var actor = systemActor.ActorOf(actorProps);
+
+            actor.Tell("1");
+            actor.Tell("2");
+            actor.Tell("3");
+            actor.Tell("4");
+
+            Console.ReadKey();
+        }
+    }
+
+    class ActorWithStatus : ReceiveActor
+    {
+        public ActorWithStatus()
+        {
+            Become(First);
+        }
+
+        void First()
+        {
+            Receive<string>(msg =>
+            {
+                Console.WriteLine($"First: {msg}");
+                Become(Second);
+            });
+        }
+
+        void Second()
+        {
+            Receive<string>(msg =>
+            {
+                Console.WriteLine($"Second: {msg}");
+                Become(First);
+            });
+        }
+    }
+}
